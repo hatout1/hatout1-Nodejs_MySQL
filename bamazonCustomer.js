8e58ca8e54f7313908c8d331b5474400eb599096
hatout01 <hatout1@gmail.com>
2019-12-11 17:51:52 -0800

    added function that grabes the selected item

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql")
+const inquirer = require("inquirer")
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,14 +12,60 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err;
     itemForSale('products');
-    connection.end()
+    // CheckIfAvailable('products');
+
 })
 
 itemForSale = obj => {
-    connection.query(`SELECT * FROM ??`, obj, (err, res) => {
+    connection.query(`SELECT item_id, product_name, price FROM ??`, obj, (err, res) => {
         if (err) throw err;
-        console.log("\nPlease see list of our produt\n")
-        console.table(res);
+        console.log("\nPlease see list of our products\n")
+        // console.table(res);
+        console.table(res)
+        askForProductId();
+
+    })
+}
+let table = 'products'
+let pickedItem;
+let quantityPicked;
+const askForProductId = () => {
+    inquirer
+        .prompt({
+            type: "input",
+            message: "Welcome again! Please enter the product's id number",
+            name: "input"
+        }).then(res => {
+            pickedItem = res.input
+            askForQuantity();
+            // console.log(res.input)
+        })
+}
+
+const askForQuantity = () => {
+    inquirer
+        .prompt({
+            type: "input2",
+            message: "Good pick! How many item you would like?",
+            name: "input2"
+        }).then(res => {
+            quantityPicked = res.input2
+            console.table("Your Total would be: $" + quantityPicked * pickedItem);
+            CheckIfAvailable();
+        })
+}
+
+
+
+CheckIfAvailable = products => {
+    connection.query(`SELECT * FROM ${table} WHERE item_id = ${pickedItem}`, products, (err, res) => {
+        if (err) throw err;
+
+        // console.table(res);
+        console.table(res)
+        // askForProductId();
+        // connection.end()
+
     })
 }
 
